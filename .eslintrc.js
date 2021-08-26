8538141d302fb62675ca27f4173dcf3a6684329b
Arek Cichocki <arexspeed95@gmail.com>
2021-08-26 22:16:08 +0200

    inserted and fix eslint default export in api

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,5 +37,17 @@ module.exports = {
         aspects: ['invalidHref', 'preferButton']
       }
     ]
-  }
+  },
+  'import/no-anonymous-default-export': [
+    'error',
+    {
+      allowArray: false,
+      allowArrowFunction: false,
+      allowAnonymousClass: false,
+      allowAnonymousFunction: false,
+      allowCallExpression: true, // The true value here is for backward compatibility
+      allowLiteral: false,
+      allowObject: false
+    }
+  ]
 };
--- pages/api/subcategory.ts
+++ pages/api/subcategory.ts
@@ -5,17 +5,15 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const db = await connectToDb();
   switch (req.method) {
     case 'GET': {
-      const data = await db.collection('categories').find().sort({ _id: 1 }).toArray();
+      const data = await db.collection('subcategories').find().sort({ _id: 1 }).toArray();
       res.json(data);
       break;
     }
     case 'POST': {
       try {
-        const payload = req.body;
-        const category = await db.collection('categories').insertOne({
-          catId: payload.catId,
-          category: payload.category
-        });
+        const payload = req.body; //payload has to be in [] cause insertMany
+        const options = { ordered: true };
+        const category = await db.collection('subcategories').insertMany(payload, options);
         res.status(200).json({ status: 'created', category });
       } catch (error) {
         res.status(422).json({ status: 'not_created', error });
