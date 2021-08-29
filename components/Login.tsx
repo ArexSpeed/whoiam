b53bf9b8e0b2431a5b96fec5dfd8dc66ef6fdf09
Arek Cichocki <arexspeed95@gmail.com>
2021-08-29 22:40:59 +0200

    admin login split to one file and components

---
+++ components/Login.tsx
@@ -0,0 +1,70 @@
+import { useRef, useState, FormEvent } from 'react';
+import { useRouter } from 'next/router';
+import { signIn } from 'next-auth/client';
+
+const Login = () => {
+  const loginForm = useRef<HTMLFormElement>(null!);
+  const [error, setError] = useState<string | null>(null);
+  const [formProcessing, setFormProcessing] = useState(false);
+  const router = useRouter();
+
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (formProcessing) return;
+    setError(null);
+    setFormProcessing(true);
+    const form = new FormData(loginForm.current);
+    const response = await signIn('credentials', {
+      redirect: false,
+      email: form.get('email'),
+      password: form.get('password')
+    });
+
+    if (response && response.ok) {
+      router.push('/admin');
+      setFormProcessing(false);
+    } else {
+      setError('Wrong login or password');
+      setFormProcessing(false);
+    }
+  };
+
+  return (
+    <form
+      className="flex flex-col justify-center items-center p-4 w-full"
+      onSubmit={handleSubmit}
+      ref={loginForm}>
+      <input
+        className="h-16 p-2 m-2 w-full bg-white rounded-sm"
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Email"
+        required
+      />
+      <input
+        className="h-16 p-2 m-2 w-full bg-white rounded-sm"
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Password"
+        required
+      />
+
+      <button
+        className="m-2 inline-flex bg-blue-500 text-white p-2 rounded-sm"
+        type="submit"
+        disabled={formProcessing}>
+        {formProcessing ? 'Process...' : 'Login'}
+      </button>
+      {error && (
+        <div className="w-full p-2 my-2 text-center text-sm bg-red-500 text-white flex flex-row justify-center items-center shadow-md rounded-md">
+          {error}
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default Login;
--- pages/admin/index.tsx
+++ pages/admin/index.tsx
@@ -1,10 +1,13 @@
 import { useEffect } from 'react';
 import type { NextPage } from 'next';
+import { useSession, signOut } from 'next-auth/client';
 import type { CategoryType, SubcategoryType, WordsType } from 'types';
 import AdminCategory from 'components/AdminCategory';
 import { getCategories, getSubcategories, getWords } from 'services/getData';
 import { useAppDispatch } from 'redux/hooks'
 import { setWords } from 'redux/slices/adminSlice'
+import Login from 'components/Login';
+import Register from 'components/Register';
 
 interface Props {
   categories: CategoryType[];
@@ -27,15 +30,40 @@ export const getServerSideProps = async () => {
 };
 
 const AdminPage: NextPage<Props>  = ({ categories, subcategories, words }) => {
+  const [session, loading] = useSession();
   const dispatch = useAppDispatch();
   // dispatch words to redux instead in all category, cause of connection number
   useEffect(() => {
     dispatch(setWords(words));
   }, [words])
-  return (
-    <div className="w-screen min-h-screen bg-green-100 flex flex-col relative font-poppins">
-      <header className="w-full text-center text-sm h-[20px] flex-none mt-2">Admin Panel</header>
+
+    return !session && !loading ? (
+      <div className="w-screen min-h-screen bg-green-100 flex flex-col relative font-poppins">
+        <header className="w-full flex justify-center h-6">
+          <p className="py-2">Login to Admin Panel</p>
+        </header>
+        <main className="w-full">
+          <section className="flex flex-col justify-center items-center m-2 p-4 bg-primary">
+            <p className="m-2 text-center">Login to your account:</p>
+            <Login />
+          </section>
+          <p className="m-2 text-center">OR</p>
+          <section className="flex flex-col justify-center items-center m-2 p-4 bg-yellow-100">
+            <p className="m-2 text-center">Create new account:</p>
+            <Register />
+          </section>
+        </main>
+      </div>
+    ) : (
+      <div className="w-screen min-h-screen bg-green-100 flex flex-col relative font-poppins">
+      <header className="w-full flex justify-center items-center relative">
+        <p className="py-2">Admin Panel</p>
+        <button className="absolute top-0 right-0 flex justify-center m-2 w-6 h-6 bg-white rounded-sm items-center" onClick={() => signOut()}>
+          <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clipRule="evenodd" /></svg>
+        </button>
+      </header>
       <main className="w-full">
+        {console.log(session, 'session')}
         <section className="m-2">Categories:</section>
         <section className="w-full flex flex-col">
           {categories?.map((category) => (
@@ -43,8 +71,8 @@ const AdminPage: NextPage<Props>  = ({ categories, subcategories, words }) => {
           ))}
         </section>
       </main>
-    </div>
-  );
+      </div>
+    )
 };
 
 export default AdminPage;
